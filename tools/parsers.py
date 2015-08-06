96d06c2c51a681ff4bf7e7662ad0b176ff1852c7
avdata99 <andres@data99.com.ar>
2015-08-05 21:54:14 -0300

    readme and fix data

--- tools/parsers.py
+++ tools/parsers.py
@@ -2,10 +2,23 @@
 """ parse info """
 
 def parse_calle(calle):
-    #TODO detectar mas errores
+    #TODO detectar mas errores, necesitamos un openRefinero
     calles={'jeronimo-l-de-cabrera': 'jeronimo-luis-de-cabrera',
             'j-l-de-cabrera': 'jeronimo-luis-de-cabrera',
             'g-l-de-cabrera': 'jeronimo-luis-de-cabrera',
+            'chacabuco-la-cruz': 'chacabuco',
+            'bindustrial-chacabuco': 'chacabuco',
+            'la-cruz-chacabuco': 'chacabuco',
+            'industrial-chacabuco': 'chacabuco',
+            'bla-cruz-chacabuco': 'chacabuco',
+            'chacabuco-industrial-': 'chacabuco',
+            'p-de-los-andes': 'paso-de-los-andes',
+            'bcruz-paso-de-los-andes': 'paso-de-los-andes',
+            'paso-de-los-andes-la-cruz': 'paso-de-los-andes',
+            'blomas-barcelona': 'barcelona',
+            'blomas-sur-barcelona': 'barcelona',
+            'd-quiros': 'duarte-quiros',
+            
             }
 
     return calles.get(calle, calle)
